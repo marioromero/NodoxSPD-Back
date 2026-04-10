458729f31c0c00f8c73ef0ac9d2f3f132f2c443c
Mario Romero <marioromero.ttp@gmail.com>
2026-04-09 21:56:41 -0400

    feat: rutas de planes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Billing\PlanController;
+use App\Http\Controllers\Billing\SubscriptionController;
+use App\Http\Controllers\Billing\PaymentController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Catálogo de planes (Puede ser pública o requerir login según decidas, usualmente es pública)
+Route::get('/plans', [PlanController::class, 'index']);
+
+// Rutas protegidas de facturación
+Route::middleware('auth:sanctum')->prefix('billing')->group(function () {
+    Route::get('/subscription', [SubscriptionController::class, 'current']);
+    Route::post('/subscribe', [SubscriptionController::class, 'store']);
+
+    Route::get('/payments', [PaymentController::class, 'index']);
+});
